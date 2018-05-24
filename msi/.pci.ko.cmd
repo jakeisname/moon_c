10fb5bb35fb08f97a1502c9af6970a59c287dc18
root <root@localhost.localdomain>
2018-05-24 08:29:39 +0000

    .

--- msi/.pci.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/moon_c/msi/pci.ko := ld -EL -r  -maarch64linux -T ./scripts/module-common.lds --build-id  -o /root/moon_c/msi/pci.ko /root/moon_c/msi/pci.o /root/moon_c/msi/pci.mod.o ;  true
--- msi/.pci.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/moon_c/msi/pci.o := ld -EL  -maarch64linux   -r -o /root/moon_c/msi/pci.o /root/moon_c/msi/gpio.o 
