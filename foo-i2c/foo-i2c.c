1b316b10150c69a659e96b94a4a7253c5fac4d65
root <root@localhost.localdomain>
2018-08-25 21:00:09 +0900

    .

--- foo-i2c/foo-i2c.c
+++ foo-i2c/foo-i2c.c
@@ -25,17 +25,14 @@ static int foo_i2c_xfer_single_msg(struct foo_i2c_dev *foo_i2c,
 
 	foo_i2c->msg = msg;
 	addr = i2c_8bit_addr_from_msg(msg);
+	dev_info(foo_i2c->device, "addr=0x%02x\n", addr >> 1);
 	tx_bytes = msg->len;
 	if (!(msg->flags & I2C_M_RD)) {
 		for (i = 0; i < tx_bytes; i++) {
 			val = msg->buf[i];
 
-			/* mark the last byte */
-			if (i == msg->len - 1)
-				val |= 1 << 31;
-
 			/* send to fifo */
-			dev_dbg(foo_i2c->device, "write=0x%02x\n", val);
+			dev_info(foo_i2c->device, "write=0x%02x\n", val);
 		}
 	}
 
@@ -44,7 +41,7 @@ static int foo_i2c_xfer_single_msg(struct foo_i2c_dev *foo_i2c,
 			msg->buf[i] = addr & 0xff;
 
 			/* receive from fifo */
-			dev_dbg(foo_i2c->device, "write=0x%02x\n", val);
+			dev_info(foo_i2c->device, "read=0x%02x\n", val);
 		}
 	}
 
@@ -60,7 +57,7 @@ static int foo_i2c_xfer(struct i2c_adapter *adapter,
 	for (i = 0; i < num; i++) {
 		ret = foo_i2c_xfer_single_msg(foo_i2c, &msgs[i]);
 		if (ret) {
-			dev_dbg(foo_i2c->device, "xfer failed\n");
+			dev_info(foo_i2c->device, "xfer failed\n");
 			return ret;
 		}
 	}
@@ -86,6 +83,7 @@ static int foo_i2c_probe(struct platform_device *pdev)
 {
 	struct foo_i2c_dev *foo_i2c;
 	struct i2c_adapter *adap;
+	int ret;
 
 	foo_i2c = devm_kzalloc(&pdev->dev, sizeof(*foo_i2c),
 				 GFP_KERNEL);
@@ -103,7 +101,12 @@ static int foo_i2c_probe(struct platform_device *pdev)
 	adap->dev.parent = &pdev->dev;
 	adap->dev.of_node = pdev->dev.of_node;
 
-	return i2c_add_adapter(adap);
+	ret = i2c_add_adapter(adap);
+
+	if (!ret)
+		dev_info(foo_i2c->device, "foo i2c driver probed success.\n");
+
+	return ret;
 }
 
 static int foo_i2c_remove(struct platform_device *pdev)
@@ -111,6 +114,8 @@ static int foo_i2c_remove(struct platform_device *pdev)
 	struct foo_i2c_dev *foo_i2c = platform_get_drvdata(pdev);
 
 	i2c_del_adapter(&foo_i2c->adapter);
+
+	dev_info(foo_i2c->device, "foo i2c driver unloaded.\n");
 
 	return 0;
 }
@@ -132,5 +137,5 @@ static struct platform_driver foo_i2c_driver = {
 module_platform_driver(foo_i2c_driver);
 
 MODULE_AUTHOR("Jake <jake@dreamwiz.com>");
-MODULE_DESCRIPTION("Jake I2C Driver");
+MODULE_DESCRIPTION("foo I2C Driver");
 MODULE_LICENSE("GPL v2");
