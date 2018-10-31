186e88193bcf06890c6feffc29c1172cf1542e2a
jakeisname <jakeisname@gmail.com>
2018-10-31 14:52:12 +0000

    .

--- test/client.c
+++ test/client.c
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/un.h>
 #include <errno.h>
@@ -12,87 +13,115 @@
 
 #define SOCK_FILE "/tmp/test_socket"
 
+#define __A1
+
 int main(int argc, char** argv)
 {
-    /* standard variables used in a udp client */
-    int                     sockfd;
-    int                     recvlen;
-#ifdef __UDP
-    struct sockaddr_in      servAddr;
+	/* standard variables used in a udp client */
+	int                     sockfd;
+	int                     recvlen;
+	struct sockaddr_un      servAddr;
+	const struct sockaddr*  servAddr_in;
+	socklen_t               servLen;
+	char                    sendLine[MAXLINE];
+	char                    recvLine[MAXLINE + 1];
+	int			    port = 0;
+	int cnt;
+	int n, len;
+
+#ifdef __A1
+	if ((sockfd = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0)
 #else
-    struct sockaddr_un      servAddr;
+	if ((sockfd = socket(AF_LOCAL, SOCK_DGRAM, 0)) < 0)
 #endif
-    const struct sockaddr*  servAddr_in;
-    socklen_t               servLen;
-    char                    sendLine[MAXLINE];
-    char                    recvLine[MAXLINE + 1];
-    int			    port = 0;
+	{
+		printf("cannot create a socket.");
+		return 1;
+	}
 
-#ifdef __UDP
-    if (argc != 3) {
-        printf("usage: udpcli <IP address> <port>\n");
-        return 1;
-    }
+	memset(&servAddr, 0, sizeof(servAddr));
 
-    port = atoi(argv[2]);
+	servAddr.sun_family = PF_LOCAL;
+	strncpy(servAddr.sun_path, SOCK_FILE, sizeof(servAddr.sun_path) - 1);
+
+	printf("created socket.\n");
+	if (connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) == -1) {
+		printf("connect() failed. errno=%d(%s)\n", errno, strerror(errno));
+		return 1;
+	}
+
+	printf("connect.\n");
+	/****************************************************************************/
+	/*               Code for sending the datagram to the server                */
+	servAddr_in = (struct sockaddr*) &servAddr;
+	servLen = sizeof(servAddr);
+
+	/* Loop while user is giving input or until EOF is read */
+	while (1) {
+#if 0
+		if (fgets(sendLine, MAXLINE, stdin) == NULL)
+			return 0;
 #else
+		sprintf(sendLine, "test-%8d\n", cnt++);
 #endif
 
-#ifdef __UDP
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		/* Attempt to send sendLine to the server */
+		len = strlen(sendLine);
+#ifdef __A1
+		printf("try to write()\n");
+		// n = write(sockfd, sendLine, len);
+		n = send(sockfd, sendLine, len, 0);
+		printf("write() n=%d\n", n);
 #else
-    if ( (sockfd = socket(AF_LOCAL, SOCK_DGRAM, 0)) < 0) {
+		if ((sendto(sockfd, sendLine, strlen(sendLine), 0, servAddr_in,
+						servLen)) == -1) 
 #endif
-       printf("cannot create a socket.");
-       return 1;
-    }
-
-    memset(&servAddr, 0, sizeof(servAddr));
-
-#ifdef __UDP
-    servAddr.sin_family = PF_INET;
-    servAddr.sin_port = htons(port);
-    inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
-#else
-    servAddr.sun_family = PF_LOCAL;
-    strncpy(servAddr.sun_path, SOCK_FILE, sizeof(servAddr.sun_path));
-    servAddr.sun_path[sizeof(servAddr.sun_path) - 1] = 0;
+		if (n != len)
+		{
+			printf("Error in sending. n=%d, err=%d(%s)\n", 
+				n, errno, strerror(errno));
+			continue;
+		}
 
 #if 0
-    if (connect(sockfd, (struct sockaddr*) &servAddr, sizeof(servAddr)) == -1) {
-            printf("connect() failed. errno=%d(%s)\n", errno, strerror(errno));
-	    return 1;
-    }
+		/* Attempt to receive recvLine from the server */
+		if ( (recvlen = recvfrom(sockfd, recvLine, MAXLINE, 0, NULL, NULL))
+				== -1) {
+			printf("Error in receiving.\n");
+		}
+
+		recvLine[recvlen] = '\0';
+		fputs(recvLine, stdout);
 #endif
-#endif
+	}
+	/*                                                                          */
+	/****************************************************************************/
 
-/****************************************************************************/
-/*               Code for sending the datagram to the server                */
-    servAddr_in = (struct sockaddr*) &servAddr;
-    servLen = sizeof(servAddr);
-
-    /* Loop while user is giving input or until EOF is read */
-    while (fgets(sendLine, MAXLINE, stdin) != NULL) {
-
-    /* Attempt to send sendLine to the server */
-        if ( ( sendto(sockfd, sendLine, strlen(sendLine) - 1, 0, servAddr_in,
-                        servLen)) == -1) {
-            printf("Error in sending.\n");
-	    continue;
-        }
-
-    /* Attempt to receive recvLine from the server */
-        if ( (recvlen = recvfrom(sockfd, recvLine, MAXLINE, 0, NULL, NULL))
-                == -1) {
-            printf("Error in receiving.\n");
-        }
-
-        recvLine[recvlen] = '\0';
-        fputs(recvLine, stdout);
-    }
-/*                                                                          */
-/****************************************************************************/
-
-    return 0;
+	return 0;
 }
 
+#if 0
+
+bytesReceived = 0;
+while (bytesReceived < BUFFER_LENGTH)
+{
+	rc = recv(sd, & buffer[bytesReceived],
+			BUFFER_LENGTH - bytesReceived, 0);
+	if (rc < 0)
+	{
+		perror("recv() failed");
+		break;
+	}
+	else if (rc == 0)
+	{
+		printf("The server closed the connection\n");
+		break;
+	}
+
+	/*****************************************************************/
+	/* Increment the number of bytes that have been received so far  */
+	/*****************************************************************/
+	bytesReceived += rc;
+}
+
+#endif
--- test/server.c
+++ test/server.c
@@ -7,189 +7,204 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <sys/time.h>
-#include "slist.c"
 #include <sys/un.h>
 #include <errno.h>
+#include <stddef.h>
 
 #define MAXLEN 40960
 
-char* toUpper(char*);
+#define __A1
+
+char *toUpper(char *);
 
 #define SOCK_FILE "/tmp/test_socket"
 
+static int read_from_client(int filedes);
+
 int main(int argc, char **argv)
 {
-	slist_t *list = (slist_t*)malloc(sizeof(slist_t));
-	slist_init(list);
-	fd_set read_fds, write_fds;				// temp file descriptors list for select()
-	int sockfd;								// socket descriptor
-#ifdef __UDP
-	struct sockaddr_in srv, cli_addr;		// srv used by bind()
-#else
-	struct sockaddr_un srv, cli_addr;		// srv used by bind()
-#endif
-	socklen_t clilen = sizeof(cli_addr);
+	fd_set read_fds, write_fds;	// temp file descriptors list for select()
+	fd_set active_fds;
+	int sockfd;			// socket descriptor
+	struct sockaddr_un srv, cli_addr;
 	int nbytes;
-	int portno = 0;	
+	int size;
+	int ret;
+	struct sockaddr_in clientname;
+	int n;
+	int i;
+	int len;
+	int peerfd;
 
-#ifdef __UDP
-	//check command line arguments 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: ./server %s <port>\n", argv[1]);
-		exit(1);
-	}
-	portno = atoi(argv[1]);
-#endif
+	unlink(SOCK_FILE);
 
-#ifdef __UDP
-	if ((sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+#ifdef __A1
+	if ((sockfd = socket(PF_LOCAL, SOCK_STREAM, 0)) < 0)
 #else
-	if ((sockfd = socket(PF_LOCAL, SOCK_DGRAM, 0)) < 0){
+	if ((sockfd = socket(PF_LOCAL, SOCK_DGRAM, 0)) < 0)
 #endif
+	{
 		perror("ERROR opening socket");
 		exit(1);
 	}
-	else
-		printf("Server : Socket() successful\n");
-    
-#ifdef __UDP
-  /* bind: use the Internet address family: */
-	/* create the socket */
-	srv.sin_family = AF_INET;
-	/* bind: socket ‘sockfd’ to port portno*/
-	srv.sin_port = htons(portno);
-	/* bind: a client may connect to any of my addresses */
-	srv.sin_addr.s_addr = htonl(INADDR_ANY);
-#else
+	
+	printf("Server : Socket() successed. sock=%d\n", sockfd);
+
 	srv.sun_family = AF_LOCAL;
 	strncpy(srv.sun_path, SOCK_FILE, sizeof(srv.sun_path));
-	srv.sun_path[sizeof(srv.sun_path) - 1] = 0;
-#endif
-     
-	if(bind(sockfd, (struct sockaddr*) &srv, sizeof(srv)) < 0){
+	size = offsetof(struct sockaddr_un, sun_path) + strlen(srv.sun_path) + 1;
+
+	if (bind(sockfd, (struct sockaddr *) &srv, size) < 0){
 		perror("ERROR on binding");
 		close(sockfd);
 		exit(1);
-	}
-	else
+	} else
 		printf("Server : bind() successful\n");
-	//Now the UDP server is ready to accept packets…
-    while(1){ 			
-		char* buf = (char*)malloc(MAXLEN);				// message buf
-		char* copybuf = (char*)malloc(MAXLEN);
-		int j;
-		memset(buf, 0, strlen(buf));
-		bzero(buf, MAXLEN);
-		FD_ZERO(&read_fds);
-		FD_SET(sockfd, &read_fds);
-		
-		// nbytes = recvfrom(sockfd, buf, MAXLEN, 0, (struct sockaddr*)&cli_addr, &clilen);
-		// printf("n=%d\n", nbytes);
 
-		int n = select(sockfd+1, &read_fds, &write_fds, 0, 0);
-		if(n < 0)
-		{
+	printf("try to listen()\n");
+	if (listen(sockfd, 10) < 0) {
+		perror("listen error");
+		exit(-1);
+	}
+	printf("listen() successed.\n");
+
+	FD_ZERO(&active_fds);
+	FD_SET(sockfd, &active_fds);
+
+	while (1) {
+		read_fds = active_fds;
+
+		printf("try to master select()\n");
+		n = select(sockfd+1, &read_fds, 0, 0, 0);
+		if (n < 0) {
 			perror("ERROR Server : select()\n");
 			close(sockfd);
 			exit(1);
 		}
-		if(FD_ISSET(sockfd, &read_fds)) {
-			printf("Server is ready to read\n");
-			nbytes = recvfrom(sockfd, buf, MAXLEN, 0, (struct sockaddr*)&cli_addr, &clilen);
-			printf("received=%d\n", nbytes);
-			if (nbytes < 0){
-				perror("ERROR in recvfrom()");
-				close(sockfd);
-				exit(1);
-			}
-#ifdef __UDP
-			j = ntohs(cli_addr.sin_port);
-#else
-			j = 0;
-#endif
-			FD_CLR(sockfd, &read_fds);
-		}
-		if(strlen(buf) != 0){		
-			memcpy(copybuf, buf, strlen(buf));
+		printf("select() successed. n=%d. FDS=%d\n", n, FD_SETSIZE);
 
+		for (i = 0; i < FD_SETSIZE; i++) {
+			if (FD_ISSET(i, &read_fds)) {
+				/* new client */
+				if (i == sockfd) {
 
-#if 0
-
-#ifdef __UDP
-			int i = strlen(buf) + 3 + 
-				strlen(inet_ntoa(cli_addr.sin_addr)) + 
-				ntohs(cli_addr.sin_port);
-#else
-			int i = strlen(buf) + 20;
-#endif
-			char* c = (char*) malloc(i);
-			char ch[5];
-#ifdef __UDP
-			strcpy(c, inet_ntoa(cli_addr.sin_addr));
-#else
-			strcat(c, "localhost");
-#endif
-			strcat(c, " : ");
-			sprintf(ch, "%d", j);
-			strcat(c, ch);
-			strcat(c, " : ");
-			strcat(c, buf);
-			slist_append(list, c);
-			//printlist(list);				//you may see the list
-#endif
-			if(list == NULL){
-				perror("List is empty. Server is not ready to write.");
-				close(sockfd);
-				exit(1);
-			}
-			else{
-				FD_ZERO(&write_fds);
-				FD_SET(sockfd, &write_fds);
-				if(FD_ISSET(sockfd, &write_fds)){
-					toUpper(copybuf);
-#ifdef __UDP
-					nbytes = sendto(sockfd, copybuf, MAXLEN, 0, 
-					    (struct sockaddr*) &cli_addr, sizeof(cli_addr));
-#else
-					nbytes = sendto(sockfd, copybuf, MAXLEN, 0, 
-					    (struct sockaddr*) &cli_addr, sizeof(cli_addr));
-#endif
-					if(nbytes < 0) {
-						printf("ERROR in sendto. err=%d(%s)",
-							errno, strerror(errno));
-						close(sockfd);
-						exit(1);
+					printf("try to accept(). i=%d\n", i);
+					len = sizeof(clientname);
+					n = accept(sockfd, (struct sockaddr *) &clientname, &size);
+					if (n < 0) {
+						perror("accept error");
+						break;
 					}
-					printf("Server is ready to write\n");
-					//printf("Send to client: %s", copybuf);
-					//printf("==========================\n");
-					printf("\n");
-					memset(&copybuf, 0, sizeof(copybuf));
-					FD_CLR(sockfd, &write_fds);
+
+					printf("accept() successed. peer=%d\n", n);
+
+					FD_SET(n, &active_fds);
+					peerfd = n;
+				} else {
+					n = read_from_client(i);
+					if (n < 0) {
+						/* remove client when lost connection*/
+						close(i);
+						FD_CLR(i, &active_fds);
+						peerfd = -1;
+					}
 				}
 			}
-
-//			bzero(ch, strlen(ch));
 		}
-		memset(&buf, 0, sizeof(buf));
-		free(buf);
-		free(copybuf);
 	}
-	free(list);
-	close(sockfd);
+
+	if (sockfd != -1)
+		close(sockfd);
+
+	if (peerfd != -1)
+		close(peerfd);
+
 	return 0;
 }
-char* toUpper(char* str){
-	if(str != NULL){	
+
+char *toUpper(char *str) 
+{
+	if (str != NULL) {	
 		int i;
-		for(i = 0; i<strlen(str); i++)
-		{
+		for (i = 0; i < strlen(str); i++) {
 			if( str[i] <= 'z' && str[i] >= 'a')
-			{
 				str[i] += 'A' - 'a';
-			}
 		}
 		return str;
 	}
+
 	return NULL;
 }
+
+static int read_from_client(int filedes)
+{
+	char buffer[MAXLEN];
+	int nbytes;
+	int seq = 0;
+	int rx_seq = 0;
+	int oops = 0;
+
+	printf("%s(%d): try to read() new_sock=%d\n", __func__, __LINE__, filedes);
+
+	nbytes = read(filedes, buffer, MAXLEN);
+	if (nbytes < 0) {
+		/* Read error. */
+		perror ("read");
+		exit (EXIT_FAILURE);
+	} else if (nbytes == 0)
+		/* End-of-file. */
+		return -1;
+
+	/* Data read. */
+	fprintf (stderr, "Server: got message: new_sock=%d, `%s'\n", filedes, buffer);
+	buffer[nbytes] = 0;
+	printf("%s: received=%d, oops=%d\n", buffer, nbytes, oops);
+
+	seq = atoi(&buffer[5]);
+	if (seq != rx_seq++) {
+		rx_seq = seq + 1;
+		oops++;
+	}
+
+	return 0;
+}
+
+#if 0
+int write_to_client()
+{
+	if (strlen(buf) == 0) 
+		return 0;
+
+	memcpy(copybuf, buf, strlen(buf));
+
+	FD_ZERO(&write_fds);
+	FD_SET(sockfd, &write_fds);
+
+	if (FD_ISSET(sockfd, &write_fds)) {
+		toUpper(copybuf);
+#ifdef __XX
+		nbytes = sendto(sockfd, copybuf, MAXLEN, 0, 
+				(struct sockaddr *) &cli_addr, sizeof(cli_addr));
+#else
+		//				nbytes = sendto(sockfd, copybuf, 1, 0, 
+		// (struct sockaddr*) &cli_addr, sizeof(cli_addr));
+		nbytes = 1;
+#endif
+		if (nbytes < 0) {
+			printf("ERROR in sendto. err=%d(%s)",
+					errno, strerror(errno));
+			close(sockfd);
+			exit(1);
+		}
+
+		//printf("Send to client: %s", copybuf);
+		//printf("==========================\n");
+		memset(&copybuf, 0, sizeof(copybuf));
+		FD_CLR(sockfd, &write_fds);
+	}
+
+	memset(&buf, 0, sizeof(buf));
+	free(buf);
+	free(copybuf);
+}
+#endif
