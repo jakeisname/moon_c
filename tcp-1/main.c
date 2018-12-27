973ec6850ffe96f86977d2b11b84b5a6a33d5a57
jake <jakeisname@gmail.com>
2018-12-27 22:26:56 +0900

    .

--- tcp-1/main.c
+++ tcp-1/main.c
@@ -15,7 +15,6 @@ peer_t clients[MAX_CLIENTS];	/* used for clients */
 int main(int argc, char **argv)
 {
 	int i;
-	int flag;
 	int port;	/* server listen port */
 
 	if (argc < 2) {
@@ -29,19 +28,20 @@ int main(int argc, char **argv)
 	if (setup_signals() != 0)
 		exit(EXIT_FAILURE);
 
+	/* bind & listen server socket */
 	if (start_listen_socket(port, &server.socket) != 0)
 		exit(EXIT_FAILURE);
 
 	/* Set nonblock for stdin. */
-	flag = fcntl(STDIN_FILENO, F_GETFL, 0);
-	flag |= O_NONBLOCK;
-	fcntl(STDIN_FILENO, F_SETFL, flag);
+	set_sock_nonblocking(STDIN_FILENO);
 
+	/* prepare clients */
 	for (i = 0; i < MAX_CLIENTS; ++i) {
 		clients[i].socket = NO_SOCKET;
 		create_peer(&clients[i]);
 	}
 
+	/* do socket processing */
 	do_server();
 }
 
