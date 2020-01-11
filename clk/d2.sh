4a9f488529884b4438d29df7e590d439ca7b5676
root <root@localhost.localdomain>
2020-01-11 19:10:42 +0900

    .

---
+++ clk/d2.sh
@@ -0,0 +1,3 @@
+echo "echo $1 > /sys/bus/platform/drivers/foo22/d2"
+echo $1 > /sys/bus/platform/drivers/foo22/d2
+./show.sh
---
+++ clk/d4.sh
@@ -0,0 +1,3 @@
+echo "echo $1 > /sys/bus/platform/drivers/foo22/d4"
+echo $1 > /sys/bus/platform/drivers/foo22/d4
+./show.sh
