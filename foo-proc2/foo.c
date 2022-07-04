9790693ee9d89ab3b96f317e5c309ffb2f60ec7b
Young-il, Moon <jakeisname@gmail.com>
2022-07-04 11:51:46 +0900

    refacto...

--- foo-proc2/foo.c
+++ foo-proc2/foo.c
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
+#include <linux/types.h>
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <asm/uaccess.h>
@@ -15,37 +16,37 @@
 #include <linux/version.h>
 
 
-#define DATA_SIZE 10
-#define MY_PROC_ENTRY1 "foo1"	/* string */
-#define MY_PROC_ENTRY2 "foo2"	/* value */
+#define BUF_SIZE	100
+#define FOO1_ENTRY	"foo1"	/* string */
+#define FOO2_ENTRY	"foo2"	/* value */
 
-static int foo1_len;
 static char *foo1_str = NULL;
+static int foo1_len;
 
 static ssize_t foo1_write(struct file *filp, 
-		const char __user * buffer, size_t count, loff_t *pos)
+		const char __user *ubuf, size_t count, loff_t *pos)
 {
 	char *data = PDE_DATA(file_inode(filp));
 
-	printk(KERN_INFO "%s(%d): count=%lu, pos=%lld, old foo_len=%d\n", 
+	pr_info("%s(%d): count=%lu, pos=%lld, old foo_len=%d\n", 
 			__func__, __LINE__, count, *pos, foo1_len);
 
-	if (count > DATA_SIZE) {
+	if (count > BUF_SIZE) {
 		return -EFAULT;
 	}
 
-	if (copy_from_user(data, buffer, count)) {
+	if (copy_from_user(data, ubuf, count)) {
 		return -EFAULT;
 	}
 
-	data[count-1] = '\0';
+	data[count] = '\0';
 
-	printk(KERN_INFO "set new foo=%s", foo1_str);
+	pr_info("set new foo=%s", data);
 
-	*pos = (int) count;
 	foo1_len = count - 1;
+	*pos = (int) count;
 
-	printk(KERN_INFO "%s(%d): count=%lu, pos=%lld, new foo_len=%d\n", 
+	pr_info("%s(%d): count=%lu, pos=%lld, new foo_len=%d\n", 
 			__func__, __LINE__, count, *pos, foo1_len);
 	return count;
 }
@@ -55,8 +56,9 @@ static ssize_t foo1_read(struct file *filp, char __user *buf,
 {
 	int err;
 	char *data = PDE_DATA(file_inode(filp));
+	int len;
 
-	printk(KERN_INFO "%s(%d): count=%lu, offp=%lld, foo_len=%d\n", 
+	pr_info("%s(%d): count=%lu, offp=%lld, foo_len=%d\n", 
 			__func__, __LINE__, count, *offp, foo1_len);
 
 	if ((int) (*offp) > foo1_len) {
@@ -71,72 +73,82 @@ static ssize_t foo1_read(struct file *filp, char __user *buf,
 		return count;	/* read 0 size, nothing to do */
 	} 
 
-	count = foo1_len + 1;	/* +1 to read the \0 */
-	err = copy_to_user(buf, data, count);	/* +1 for \0 */
-	printk(KERN_INFO "get foo=%s\n", buf);
-	*offp = count;
-
+	len = foo1_len + 1;	/* +1 to read the \0 */
+	err = copy_to_user(buf, data, len);	/* +1 for \0 */
 	if (err) {
-		printk(KERN_INFO "Error in copying data.");
+		pr_info("Error in copying data.");
 	}
 
-	printk(KERN_INFO "%s(%d): count=%lu, offp=%lld, foo_len=%d\n", 
-			__func__, __LINE__, count, *offp, foo1_len);
+	*offp = len;
 
-	return count;
+	return len;
 }
 
-static int foo2 = 0;
-static int foo2_len;
 static char *foo2_str = NULL;
+static int foo2_int = 0;
+static int foo2_len;
 
-static ssize_t foo2_write(struct file *filp, 
-		const char __user * buffer, size_t count, loff_t *pos)
+static ssize_t proc_int_write(struct file *filp, 
+		const char __user *ubuf, size_t count, loff_t *pos,
+		int *dest, int *dest_len)
 {
+	int num;
 	char *data = PDE_DATA(file_inode(filp));
-	char tmp[100];
+	int len;
 
-	printk(KERN_INFO "%s(%d): count=%lu, pos=%lld, old foo2_len=%d\n", 
+	pr_info("%s(%d): count=%lu, pos=%lld, old foo2_len=%d\n", 
 			__func__, __LINE__, count, *pos, foo2_len);
 
-	if (count > DATA_SIZE) {
+	if (count > BUF_SIZE) {
 		return -EFAULT;
 	}
 
-	if (copy_from_user(data, buffer, count)) {
+	if (copy_from_user(data, ubuf, count)) {
 		return -EFAULT;
 	}
 
 	data[count-1] = '\0';
 
-	sscanf(data, "%d", &foo2);
-	foo2_len = sprintf(tmp, "%d\n", foo2);
-	foo2_len--;
+	num = sscanf(data, "%d", dest);
+	if (num != 1)
+		return -EFAULT;
 
-	printk(KERN_INFO "set new foo2=%d", foo2);
+	len = sprintf(data, "%d\n", *dest);
+	data[len] = 0;
 
-	*pos = (int) count;
+	pr_info("set new val=%d\n", *dest);
 
-	printk(KERN_INFO "%s(%d): count=%lu, pos=%lld, new foo2_len=%d\n", 
-			__func__, __LINE__, count, *pos, foo2_len);
-	return count;
+	*dest_len = len - 1;
+
+	*pos = (loff_t) len;
+
+	pr_info("%s(%d): count=%lu, pos=%lld, new val_len=%d\n", 
+			__func__, __LINE__, count, *pos, *dest_len);
+
+	return len;
 }
 
-static ssize_t foo2_read(struct file *filp, char __user *buf, 
-		size_t count, loff_t *offp)
+static ssize_t foo2_write(struct file *filp, 
+		const char __user *ubuf, size_t count, loff_t *pos)
 {
-	int err;
+	return proc_int_write(filp, ubuf, count, pos, &foo2_int, &foo2_len);
+}
+
+static ssize_t proc_int_read(struct file *filp, char __user *ubuf, 
+		size_t count, loff_t *offp, int val, int val_len)
+{
 	char *data = PDE_DATA(file_inode(filp));
-	int temp_len;
+	ssize_t size;
+	int n;
 
-	printk(KERN_INFO "%s(%d): count=%lu, offp=%lld, foo2_len=%d\n", 
-			__func__, __LINE__, count, *offp, foo2_len);
+	pr_info("%s(%d): count=%lu, offp=%lld, val_len=%d\n", 
+			__func__, __LINE__, count, *offp, val_len);
 
-	if ((int) (*offp) > foo2_len) {
+	if ((int) (*offp) > val_len) {
 		return 0;
 	}
 
-	if (!(data)) {
+	if (data == NULL) {
 		return 0;
 	}
 
@@ -144,22 +156,24 @@ static ssize_t foo2_read(struct file *filp, char __user *buf,
 		return count;	/* read 0 size, nothing to do */
 	} 
 
-	temp_len = sprintf(data, "%d\n", foo2);
+	size = sprintf(data, "%d\n", val);
+	size++;
 
-	count = temp_len + 1;	/* +1 to read the \0 */
-	err = copy_to_user(buf, data, count);	/* +1 for \0 */
-
-	printk(KERN_INFO "get foo2=%d\n", foo2);
-	*offp = count;
-
-	if (err) {
-		printk(KERN_INFO "Error in copying data.");
+	n = copy_to_user(ubuf, data, size);	/* +1 for \0 */
+	if (n) {
+		pr_err("copy_to_use() failed\n");
+		return -EFAULT;
 	}
 
-	printk(KERN_INFO "%s(%d): count=%lu, offp=%lld, foo2_len=%d\n", 
-			__func__, __LINE__, count, *offp, foo2_len);
+	*offp = size;
 
-	return count;
+	return size;
+}
+
+static ssize_t foo2_read(struct file *filp, char __user *buf, 
+		size_t count, loff_t *offp)
+{
+	return proc_int_read(filp, buf, count, offp, foo2_int, foo2_len);
 }
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
@@ -183,50 +197,109 @@ static struct file_operations foo2_proc_fops = {
 #endif
 
 
-static int create_new_proc_entry(void) 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+static int proc_create_str_and_init(char *name, umode_t mode,
+		struct proc_dir_entry *dir, const struct proc_ops *ops,
+		char *init_value, int *val_len, char *data_str)
+#else
+static int proc_create_str_and_init(char *name, umode_t mode,
+		struct proc_dir_entry *dir, const struct file_operations *ops,
+		char *init_value, int *val_len, char *data_str)
+#endif
 {
-	struct proc_dir_entry *proc1;
-	struct proc_dir_entry *proc2;
-	char *DATA1 = "foo";
-	char *DATA2 = "0";
+	struct proc_dir_entry *f1;
 
-	foo1_len = strlen(DATA1);
-	foo1_str = kmalloc((size_t) DATA_SIZE, GFP_KERNEL);
-	if (foo1_str == NULL) {
-		goto err4;
-	}
-
-	strncpy(foo1_str, DATA1, foo1_len + 1);
-
-	proc1 = proc_create_data(MY_PROC_ENTRY1, 0666, NULL, 
-			&foo1_proc_fops, foo1_str);
-	if (proc1 == NULL) {
-		goto err3;
-	}
-
-	foo2_len = strlen(DATA2);
-	foo2_str = kmalloc((size_t) DATA_SIZE, GFP_KERNEL);
-	if (foo2_str == NULL) {
+	pr_err("%s(%d): Jake\n", __func__, __LINE__);
+	data_str = kmalloc((size_t) BUF_SIZE, GFP_KERNEL);
+	if (data_str == NULL) {
 		goto err2;
 	}
 
-	strncpy(foo2_str, DATA2, foo2_len + 1);
+	if (init_value) {
+		*val_len = strlen(init_value);
+		strncpy(data_str, init_value, *val_len + 1);
+	} else {
+		*val_len = 0;
+		data_str[0] = 0;
+	}
 
-	proc2 = proc_create_data(MY_PROC_ENTRY2, 0666, NULL, 
-			&foo2_proc_fops, foo2_str);
-	if (proc2 == NULL) {
+	f1 = proc_create_data(name, mode, dir, ops, data_str);
+	if (!f1) {
+		pr_err("Failed to create %s\n", name);
+		goto err;
+	}
+
+	return 0;
+
+err:
+	kfree(data_str);
+
+err2:
+	return -1;
+}
+
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+static int proc_create_int_and_init(char *name, umode_t mode,
+		struct proc_dir_entry *dir, const struct proc_ops *ops,
+		int init_value, int *val_len, char *data_str)
+#else
+static int proc_create_int_and_init(char *name, umode_t mode,
+		struct proc_dir_entry *dir, const struct file_operations *ops,
+		int init_value, int *val_len, char *data_str)
+#endif
+{
+	struct proc_dir_entry *f1;
+
+	pr_err("%s(%d): Jake\n", __func__, __LINE__);
+	data_str = kmalloc((size_t) BUF_SIZE, GFP_KERNEL);
+	if (data_str == NULL) {
+		goto err2;
+	}
+
+	*val_len = sprintf(data_str, "%d", init_value);
+	data_str[*val_len] = 0;
+
+	f1 = proc_create_data(name, mode, dir, ops, data_str);
+	if (!f1) {
+		pr_err("Failed to create %s\n", name);
+		goto err;
+	}
+
+	return 0;
+
+err:
+	kfree(data_str);
+
+err2:
+	return -1;
+}
+
+
+int create_new_proc_entry(void) 
+{
+	int ret;
+
+	ret = proc_create_str_and_init(FOO1_ENTRY, 0666, NULL, &foo1_proc_fops,
+			foo1_str, &foo1_len, foo1_str);
+	if (ret) {
+		pr_err("Failed to create %s\n", FOO1_ENTRY);
+		goto err2;
+	}
+
+	ret = proc_create_int_and_init(FOO2_ENTRY, 0666, NULL, &foo2_proc_fops,
+			foo2_int, &foo2_len, foo2_str);
+	if (ret) {
+		pr_err("Failed to create %s\n", FOO2_ENTRY);
 		goto err1;
 	}
 
 	return 0;
 
 err1:	
-	kfree(foo2_str);
-err2:
-	remove_proc_entry(MY_PROC_ENTRY1, NULL);
-err3:
+	remove_proc_entry(FOO1_ENTRY, NULL);
 	kfree(foo1_str);
-err4:
+err2:
 	return -1;
 }
 
@@ -242,8 +315,11 @@ int foo_init (void)
 
 void foo_exit(void) 
 {
-	remove_proc_entry(MY_PROC_ENTRY1, NULL);
-	remove_proc_entry(MY_PROC_ENTRY2, NULL);
+	remove_proc_entry(FOO1_ENTRY, NULL);
+	remove_proc_entry(FOO2_ENTRY, NULL);
+
+	kfree(foo1_str);
+	kfree(foo2_str);
 }
 
 MODULE_LICENSE("GPL");
